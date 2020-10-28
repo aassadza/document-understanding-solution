01d11dbf5f7c6b758ad68bdba2728aded9f815bd
Alex Chirayath <60116912+alexchirayath@users.noreply.github.com>
2020-10-27 17:17:16 -0700

    Revert "Update/dataset (#138)"
    This reverts commit f38ed9052fc0d2c369a227ef2baa6508aa016215.

--- source/app/pages/documents/index.js
+++ source/app/pages/documents/index.js
@@ -167,12 +167,9 @@ function Documents({
           className={css.searchBar}
           light
           suggestions={ENABLE_KENDRA && [
-            'What is diabetes?',
-            'What are the types of diabetes?',
-            'How many people have diabetes?',
-            'How to manage kidney disease',
-            'Does high blood pressure cause kidney disease?',
-            'How to prevent kidney disease'
+            'What is Muscular Dystrophy?',
+            'Is there a cure for Muscular Dystrophy?',
+            'Is Muscular Dystrophy rare?'
           ]}
         />
       </div>
